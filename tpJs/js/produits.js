df27cac4df2709851bb4de8e75a681043cd8467d
didier <didier@d-defrance.fr>
2020-09-04 15:07:17 +0200

    ajax avec communication http serveur

--- tpJs/js/produits.js
+++ tpJs/js/produits.js
@@ -1,14 +1,21 @@
 $(function(){
 	
 	  $('#idBtn').on("click",function(){
-	  
+		  
+		  var produitsUrl = $('#idSelUrl').val();
+		  var categorie = $('#idCategorie').val();
+		  if(categorie!=""){
+			  produitsUrl=produitsUrl+"?categorie="+categorie;
+		  }
+	      console.log("produitsUrl="+produitsUrl);
+		  
 	      $.ajax({
 			type: "GET",
-			url: "./data/products.json",
+			url: produitsUrl,
 			contentType : "application/json",
 			success: function (data,status,xhr) {
 				if (data) {
-				    console.log("data=" + JSON.stringify(data));
+				    //console.log("data=" + JSON.stringify(data));
 					var tabProduits = data;
 					$("#tBodyProd").empty();
 					for(i in tabProduits){
@@ -17,8 +24,7 @@ $(function(){
 									+"</td><td>"+tabProduits[i].prix
 									+"</td><td>"+tabProduits[i].categorie
 									+"</td></tr>";
-						console.log("nouvelleLigne=" + nouvelleLigne);
-						//$('#tableHtmlProd').append(nouvelleLigne);
+						//console.log("nouvelleLigne=" + nouvelleLigne);
 						$('#tBodyProd').append(nouvelleLigne);
 					}
 					}
